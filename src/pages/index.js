bce51f25b34a75ac6fda1a420612e6c721164371
Lonnie P <allon.paz@gmail.com>
2019-12-03 20:41:05 -0700

    second commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import { Link, graphql } from "gatsby";
 
 import Layout from "../components/layout";
 import SEO from "../components/seo";
-// import Hero from '../components/Globals/Hero';
+import Hero from '../components/Globals/Hero';
 import Info from '../components/home/Info';
 import BackgroundSection from "../components/Globals/BackgroundSection";
 
@@ -13,39 +13,39 @@ const IndexPage = ({data}) => (
     title="Home" 
     keywords={[`gatsby`, `application`, `react`]} 
     />
-    {/* <Hero /> */}
+    <Hero />
     <Info />
-    <BackgroundSection img={data.img.childImageSharp.fluid} title="Coffee Xchange" styleClass="default-background" />
+    {/* <BackgroundSection img={data.img.childImageSharp.fluid} title="Coffee Xchange" styleClass="default-background" /> */}
   </Layout>
 );
 
-export const query = graphql`{
-    img:file(relativePath:{eq:"cbg2.jpg"}) {
-      childImageSharp {
-        fluid(maxWidth: 400, maxHeight: 250) {
-          ...GatsbyImageSharpFluid
-        }
-      }
-    }
-    menu:allContentfulCoffeeItem{
-    edges{
-      node{
-        id
-        title
-        description{
-          description
-        }
-        price
-        category
-        image{
-          fixed(width:50,height:50){
-            ...GatsbyContentfulFixed_tracedSVG
-          }
-        }
-      }
-    }
-  }
-  }`
+// export const query = graphql`{
+//     img:file(relativePath:{eq:"cbg2.jpg"}) {
+//       childImageSharp {
+//         fluid(maxWidth: 400, maxHeight: 250) {
+//           ...GatsbyImageSharpFluid
+//         }
+//       }
+//     }
+//     menu:allContentfulCoffeeItem{
+//     edges{
+//       node{
+//         id
+//         title
+//         description{
+//           description
+//         }
+//         price
+//         category
+//         image{
+//           fixed(width:50,height:50){
+//             ...GatsbyContentfulFixed_tracedSVG
+//           }
+//         }
+//       }
+//     }
+//   }
+//   }`
 
 
 export default IndexPage
