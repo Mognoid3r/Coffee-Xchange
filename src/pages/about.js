402a44db0ab81252be18e6704d60aed1c9d74366
Mognoid3r <48867481+Mognoid3r@users.noreply.github.com>
2019-12-05 11:15:27 -0700

    testing this commit

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,37 +1,35 @@
-import React from "react";
-import { Link, graphql } from "gatsby";
+import React from "react"
+import { graphql } from "gatsby"
 
-import Layout from "../components/layout";
-import SEO from "../components/seo";
-import Hero from '../components/Globals/Hero';
-// import Info from '../components/home/Info';
-// import BackgroundSection from "../components/Globals/BackgroundSection";
-// import { FaGulp } from "react-icons/fa";
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+import BackgroundSection from "../components/Globals/BackgroundSection"
+import Info from "../components/Home/Info"
 
-const AboutPage = ({data}) => (
-  <Layout>
-    <SEO 
-    title="About" 
-    keywords={[`gatsby`, `application`, `react`]} 
-    />
-    <Hero />
-    {/* <Info />  */}
-    {/* <BackgroundSection 
-    img={data.img.childImageSharp.fluid} 
-    title="About Us" 
-    styleClass="about-background" /> */}
-  </Layout>
-);
+const IndexPage = ({ data }) => {
+  return (
+    <Layout>
+      <SEO title="About" keywords={[`gatsby`, `application`, `react`]} />
+      <BackgroundSection
+        img={data.img.childImageSharp.fluid}
+        title="about us"
+        styleClass="about-background"
+      />
+      <Info />
+    </Layout>
+  )
+}
 
-// export const query = graphql`{
-//     img:file(relativePath:{eq:"cbg2.jpg"}) {
-//       childImageSharp {
-//         fluid(maxWidth: 400, maxHeight: 250) {
-//           ...GatsbyImageSharpFluid
-//         }
-//       }
-//     }
-//   }`
+export const query = graphql`
+  {
+    img: file(relativePath: { eq: "cbg1.jpg" }) {
+      childImageSharp {
+        fluid {
+          ...GatsbyImageSharpFluid_tracedSVG
+        }
+      }
+    }
+  }
+`
 
-
-export default AboutPage
+export default IndexPage
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,52 +1,58 @@
-import React from "react";
-import { Link, graphql } from "gatsby";
+import React from "react"
+import { graphql } from "gatsby"
 
-import Layout from "../components/layout";
-import SEO from "../components/seo";
-import Hero from '../components/Globals/Hero';
-import Info from '../components/home/Info';
-import BackgroundSection from "../components/Globals/BackgroundSection";
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+import BackgroundSection from "../components/Globals/BackgroundSection"
+import Info from "../components/Home/Info"
+import Menu from "../components/Home/Menu"
+import Products from "../components/Home/Products"
+import Contact from "../components/Home/Contact"
+const IndexPage = ({ data }) => {
+  return (
+    <Layout>
+      <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
+      <BackgroundSection
+        img={data.img.childImageSharp.fluid}
+        title="Coffee Xchange"
+        styleClass="default-background"
+      />
+      <Info />
+      <Menu items={data.menu} />
+      <Products />
+      <Contact />
+    </Layout>
+  )
+}
 
-const IndexPage = ({data}) => (
-  <Layout>
-    <SEO 
-    title="Home" 
-    keywords={[`gatsby`, `application`, `react`]} 
-    />
-    <Hero />
-    <Info />
-    {/* <BackgroundSection img={data.img.childImageSharp.fluid} title="Coffee Xchange" styleClass="default-background" /> */}
-  </Layout>
-);
-
-// export const query = graphql`{
-//     img:file(relativePath:{eq:"cbg2.jpg"}) {
-//       childImageSharp {
-//         fluid(maxWidth: 400, maxHeight: 250) {
-//           ...GatsbyImageSharpFluid
-//         }
-//       }
-//     }
-//     menu:allContentfulCoffeeItem{
-//     edges{
-//       node{
-//         id
-//         title
-//         description{
-//           description
-//         }
-//         price
-//         category
-//         image{
-//           fixed(width:50,height:50){
-//             ...GatsbyContentfulFixed_tracedSVG
-//           }
-//         }
-//       }
-//     }
-//   }
-//   }`
-
+export const query = graphql`
+  {
+    img: file(relativePath: { eq: "cbg2.jpg" }) {
+      childImageSharp {
+        fluid {
+          ...GatsbyImageSharpFluid_tracedSVG
+        }
+      }
+    }
+    menu: allContentfulCoffeeItem {
+      edges {
+        node {
+          id
+          title
+          description {
+            description
+          }
+          price
+          category
+          image {
+            fixed(width: 50, height: 50) {
+              ...GatsbyContentfulFixed_tracedSVG
+            }
+          }
+        }
+      }
+    }
+  }
+`
 
 export default IndexPage
-
